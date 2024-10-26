ec6a811fa9e13c2554d6bb15b8adf2913fa3423a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-26 05:38:26 +0200

    gta9: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-X115 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gta9.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gta9-user \
+    omni_gta9-userdebug \
+    omni_gta9-eng
